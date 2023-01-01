ab17cbb3eb82324d3ac515c9f743beb55af93057
chinmayshewale <shewalechinmay54@gmail.com>
2023-01-01 20:30:35 +0530

    Succesfully used refresh token

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/callback', async (req, res) => {
   const code = req.query.code;
   console.log("state-"+state);
   console.log("code-"+code);
-  let codeVerifier='Wv-JO8iv00gZxq~0emtd.EO3Au5d1pFPZcVhsRtvnXQ6DOSI34aoLhdfzCNnurrwbv58mOxCCORRD50aY.AL7r-86zVIxMbv6BKKIqFZWlS7gyOk87J877d4TNezQHP7';
+  let codeVerifier='JAt9PkjYMDrPF5CS3eWRNSV7Jsorf4VPQ27WHD5.pQ9f8s9Fcb2-aY_jd7hJYBo-63GoTE~DcWCn5FNrDb_tsbX2sfRWrVG9j2s6dWaFKiaqVnMH~wztV-Ep7hQ8D1sD';
   console.log("verifier-"+codeVerifier);
   
   const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -12,50 +12,60 @@ async function sendDM(message, userId) {
     console.log(pRefreshToken);
     console.log(pAccesstoken);
 
-    // try {
-    //     console.log("try block");
-    //     const v2client = new TwitterApi(pAccesstoken)
-    //     console.log("Direct user created");
-    //     console.log(v2client);
-    //     const { dm_conversation_id, dm_event_id } = await v2client.v2.sendDmToParticipant(userId, {
-    //         text: message,
-    //     });
-    //     console.log("message sent");
-
-
-    // }
-    // catch (err) {
-    console.log("error");
-    // console.log(err);
-    const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
-    console.log(client);
-    // const client = new TwitterApi(pAccesstoken);
-    console.log("Client created");
-    console.log(pRefreshToken);
     try {
-        const { client: refreshedClient, accessToken, pRefreshToken: newRefreshToken } = await client.refreshOAuth2Token(pRefreshToken)
-            .then(obj => { console.log("obj: "+obj); });
-        console.log("token refreshed");
-        console.log(pRefreshToken);
-        console.log(accessToken);
-        const userDoc = Admin.updateOne({ user: 'All details' }, { oauth_acces_token: accessToken, oauth_refresh_token: newRefreshToken })
-            .then(obj => { console.log(obj) })
-            .catch(err => { console.log(err) });
-        // console.log("No refresh error");
+        // console.log("try block");
+        const v2client = new TwitterApi(pAccesstoken)
+        console.log("Direct user created");
+        // console.log(v2client);
+        const sent1 = await v2client.v2.sendDmToParticipant(userId, {
+            text: message,
+        }).then(result => {
+            console.log("message sent");
+        }
+        );
 
-        const { dm_conversation_id, dm_event_id } = refreshedClient.v2.sendDmToParticipant(userId, {
-            text: "Refreshed token ",
-        }).then(obj => { console.log("Message sent") })
-            .catch(err => {
-                console.log(err);
-            });
 
     }
     catch (err) {
-        console.log("Main client error");
+        console.log("error");
         console.log(err);
+        const v2client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
+        console.log("Client created");
+        let refreshToken;
+        let accessToken;
+        try {
+            const refresh = await v2client.refreshOAuth2Token(pRefreshToken)
+                .then(obj => {
+                    let body = JSON.stringify(obj)
+                    let json = JSON.parse(body);
+                    // console.log(body);
+                    // console.log(json);
+                    refreshToken = json.refreshToken;
+                    accessToken = json.accessToken;
+                    console.log("Refresh token: " + refreshToken);
+                    console.log("Access Token: " + accessToken);
+                    console.log("token refreshed");
+                });
+
+            const refreshedClient = new TwitterApi(accessToken);
+            const userDoc = Admin.updateOne({ user: 'All details' }, { oauth_acces_token: accessToken, oauth_refresh_token: refreshToken })
+                .then(obj => {
+                    console.log(obj)
+                })
+                .catch(err => { console.log(err) });
+            const sent2 = refreshedClient.v2.sendDmToParticipant(userId, {
+                text: message,
+            }).then(obj => { console.log("Message sent") })
+                .catch(err => {
+                    console.log(err);
+                });
+
+        }
+        catch (err) {
+            console.log("Main client error");
+            console.log(err);
+        }
     }
-    // }
 
 }
 
@@ -88,21 +98,21 @@ exports.post = (req, res, next) => {
     var body = req.body; //store the body of the request
     // console.log(process.env.USER_ID);
     if (body.direct_message_events) {
-        console.log(body);
+        // console.log(body);
         let recipientId = body.direct_message_events[0].message_create.target.recipient_id;
         let senderId = body.direct_message_events[0].message_create.sender_id;
         let user = body.users;
-        console.log("useer: " + user);
+        let stringify = JSON.stringify(user);
+        // console.log(stringify);
+        // console.log("useer: " + user);
         console.log("Sender Id: " + senderId);
-        console.log("Self created");
         if (senderId != "1606266324094955521") {
-            console.log("Not self created");
             let message_data = body.direct_message_events[0].message_create.message_data.text;//storing the message text using json sent by twitter
             let urls = body.direct_message_events[0].message_create.message_data.entities.urls;
             let hashtags = body.direct_message_events[0].message_create.message_data.entities.hashtags;
             // console.log(hashtags);
             console.log("----");
-            // console.log(urls);
+            console.log(urls);
             message_data = message_data.trimStart();
             message_data = message_data.toLowerCase();
             message_data = message_data.trimEnd();
